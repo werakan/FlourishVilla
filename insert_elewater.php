3fd25b8e6430d5eaea12e2fa125c1d45f5a0ebf1
Werakan <werakan@outlook.com>
2016-06-17 21:52:14 +0700

    545555

--- insert_elewater.php
+++ insert_elewater.php
@@ -6,10 +6,7 @@ $date = date("Y-m-d H:i:s");
 $billele = $_POST['billele'];
 $billwater = $_POST['billwater'];
 $rentid = $_POST['rentid'];
-
-echo "roomid: ".$rentid;
-
-
+$dexp=strtotime("tomorrow");
 
 $check_billele = false;
 $check_billwater = false;
@@ -29,23 +26,25 @@ if($rentid!=null && strlen($rentid)>=1){
 
 
 if($check_billwater == true && $check_billele == true  && $check_rentid == true ){
+echo "roomid: ".$rentid;
+echo "water: ".$billwater;
+echo "ele: ".$billele;
+echo date("Y-m-d h:i:sa", $dexp);	
 
 	$a = "INSERT INTO bill (bill_ele,
-	bill_water,bill_date ,bill_rent,
-	) VALUES(	
+	bill_water,bill_date ,bill_rent,bill_exp ) VALUES(	
 																																	  
 '$billele',
 																																									'$billwater',
 																																									'$date',
-
-																																									
-																																									'$rentid' )";
+																																																																																'$rentid'
+																																																																																'$dexp' )";
 																																									 mysql_query($a);	
 																																										
 
 
 
-																																									 header("location: http://www.allprojectz.com/max/elewater_admin2.php?message=OK");																																				
+																																									 // header("location: http://www.allprojectz.com/max/elewater_admin2.php?message=OK");																																				
 }else{
  header("location: http://www.allprojectz.com/max/elewater_admin2.php?message=FILL");	
 	
